e835d07cf8afb0df2771645ae68e55577ddc9e74
quanph <code.tieumomo@gmail.com>
2023-08-01 19:34:19 +0700

    ✨ feat(app): add seo image

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DocsThemeConfig } from "nextra-theme-docs";
+import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import Image from "next/image";
 import Logo from "./assets/rocket.gif";
 import { useRouter } from "next/router";
@@ -33,14 +33,29 @@ const config: DocsThemeConfig = {
   //     </a>
   //   ),
   // },
-  head: (
-    <>
-      <link
-        rel="icon"
-        href="https://img.icons8.com/fluency/48/copybook--v2.png"
-      ></link>
-    </>
-  ),
+  head: function useHead() {
+    const { frontMatter } = useConfig();
+
+    const imageLink =
+      frontMatter.image ??
+      "https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg?w=1380&t=st=1690891839~exp=1690892439~hmac=27f746530018fbd24b7aa5952c4252b1ab25259c9d77f4086090d5fca082ca00";
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:image" content={imageLink} />
+        <link
+          rel="icon"
+          href="https://img.icons8.com/fluency/48/copybook--v2.png"
+        ></link>
+        <meta name="og:image" content={imageLink} />
+      </>
+    );
+  },
   sidebar: {
     titleComponent({ title, type }) {
       if (type === "separator") {
