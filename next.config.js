62ce824da90cbfc951786d7bb6dde572a614d20c
code-tieumomo <code.tieumomo@gmail.com>
2023-11-07 17:18:06 +0700

    update: snippets

--- next.config.js
+++ next.config.js
@@ -1,13 +1,21 @@
-const { remarkCodeHike } = require("@code-hike/mdx");
+// const { remarkCodeHike } = require("@code-hike/mdx");
+const { readFileSync } = require("fs");
 
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
   mdxOptions: {
-    remarkPlugins: [
-      [remarkCodeHike, { theme: "one-dark-pro", showCopyButton: true }]
-    ]
-  }
+    // remarkPlugins: [
+    //   [remarkCodeHike, { theme: "one-dark-pro", showCopyButton: true }]
+    // ]
+    rehypePrettyCodeOptions: {
+      // VSCode theme or built-in Shiki theme, see Shiki documentation for more information
+      theme: JSON.parse(
+        readFileSync("./public/syntax/one_dark_pro.json", "utf8")
+      )
+    }
+  },
+  defaultShowCopyCode: true
 });
 
 module.exports = withNextra();
